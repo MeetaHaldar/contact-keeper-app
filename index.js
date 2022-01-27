54ce090840b1e907048019f6247a065e5d0e8ae4
meeta haldar <meetahaldar1001@gmail.com>
2022-01-28 01:35:25 +0530

    enable delete and edit feature

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 let express = require("express");
+
+let mongodb = require("mongodb").MongoClient;
+let ObjectId = require("mongodb").ObjectId;
+
+let db;
 let app = express();
+app.use(express.static('public'))
+
+
+let connectionString =
+  "mongodb+srv://tarang:AzOgcKjwqupEmWb6@cluster0.9s75g.mongodb.net/contactPageApp?retryWrites=true&w=majority";
+
+mongodb.connect(
+  connectionString,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (err, client) {
+    if (err) throw err;
+    else {
+      db = client.db("contactPageApp");
+      const port = process.env.PORT || 4000;
+      app.listen(port);
+    }
+  }
+);
+
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
 app.get("/", function (req, res) {
-  res.send(`<!DOCTYPE html>
+  db.collection('items').find().toArray(function(err,items){
+    // console.log(items)
+
+      res.send(`<!DOCTYPE html>
     <html>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
         <script src="https://cdn.tailwindcss.com"></script>
         <style>
           body {
@@ -15,7 +47,9 @@ app.get("/", function (req, res) {
           * {
             box-sizing: border-box;
           }
-    
+    i{
+      cursor: pointer;
+    }
           /* Button used to open the contact form - fixed at the bottom of the page */
           .open-button {
             background-color: #555;
@@ -90,31 +124,7 @@ app.get("/", function (req, res) {
       </head>
       <body>
         <section class="bg-gray-100 h-screen">
-          <h1 class="text-center text-3xl py-4 w-6/12 text-blue-700 mx-auto ">
-            Contact Keeper App
-          </h1>
-          <table class="text-center py-4 w-max mx-auto my-4 text-black">
-            <thead class="text-center py-4 w-max bg-blue-200 mx-auto my-4 text-black">
-              <tr>
-                <th >Name</th>
-                <th>Phone no.</th>
-                <th>Email</th>
-                <th>Edit</th>
-                <th>Delete</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Meeta</td>
-                <td>89695794797</td>
-                <td>meet@4.</td>
-                <td><button>edit</button></td>
-                <td><button>delete</button></td>
-              </tr>
-            </tbody>
-          </table>
-      
-    
+     
         <button class="open-button" onclick="openForm()">Add Contact</button>
     
         <div class="form-popup" id="myForm">
@@ -135,29 +145,68 @@ app.get("/", function (req, res) {
             </button>
           </form>
         </div>
+
+        <h1 class="text-center text-3xl py-4 w-6/12 text-blue-700 mx-auto ">
+        Contact Keeper App
+      </h1>
+      <table class="text-center py-4 w-max mx-auto my-4 text-black">
+        <thead class="text-center py-4  w-max bg-blue-200 mx-auto my-4 text-black">
+          <tr>
+            <th >Name</th>
+            <th>Phone no.</th>
+            <th>Email</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+       ${items.map(function(x){
+         return ` <tbody>
+         <tr class="p-4">
+
+           <td class="mx-4"><p>${x.name}</p><i class="fas fa-pen edit-name p-2" data-id="${x._id}"></i></td>
+           <td>${x.phone} <i class="fas fa-pen edit-phone p-2" data-id="${x._id}"></i></td>
+           <td>${x.email} <i class="fas fa-pen edit-email p-2" data-id="${x._id}"></i></td>
+  
+           <td><i class="fas fa-trash-alt delete-me" data-id="${x._id}"></i></td>
+         </tr>
+       </tbody>`
+       }).join(" ")}
+      </table>
+  
+
     </section>
-        <script>
-        function openForm() {
-            document.getElementById("myForm").style.display = "block";
-          }
-          
-          function closeForm() {
-            document.getElementById("myForm").style.display = "none";
-          }
+        <script src="/browser.js">
+                                       
         </script>
+        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
       </body>
     </html>
     `);
+  })
+
 });
 
 app.post("/create-item", function (req, res) {
-  console.log(req.body.item);
-  console.log(req.body.phn);
-  console.log(req.body.email);
-
-  res.send("thankyou for submission");
+  db.collection("items").insertOne(
+    { name: req.body.item, phone: req.body.phn, email: req.body.email },
+    function () {
+res.redirect('/')    }
+  );
 });
 
-app.listen(5000, function () {
-  console.log("your app is running on port 5000");
-});
+
+
+app.post('/update-item',function(req,res){
+db.collection('items').findOneAndUpdate({_id:ObjectId(req.body.id)},{$set:{name:req.body.text}},function(){
+ 
+  console.log(ObjectId(req.body.id))
+      res.send("success")
+    })
+})
+
+
+app.post('/delete-item',function(req,res){
+  // db.collection('items').deleteOne({_id:ObjectId(req.body.id)},function(){
+  //       res.send("success")
+  //     })
+  })
---
+++ public/browser.js
@@ -0,0 +1,73 @@
+document.addEventListener("click", function (e) {
+  //delete feature
+
+  if (e.target.classList.contains("delete-me")) {
+    if (confirm("do you really want to delete??")) {
+      axios
+        .post("/delete-item", { id: e.target.getAttribute("data-id") })
+        .then(function () {
+          e.target.parentElement.parentElement.parentElement.remove();
+        })
+        .catch(function () {
+          console.log("please try agin later");
+        });
+    }
+  }
+
+  //update feature
+  if (e.target.classList.contains("edit-name")) {
+    let userInput = prompt(
+      "enter your new name",
+      e.target.previousSibling.innerHTML
+    );
+
+    let q = e.target.getAttribute("data-id");
+    if (userInput) {
+      axios
+        .post("/update-item", { text: userInput, id: q })
+        .then(function () {
+          e.target.previousSibling.innerHTML = userInput;
+          console.log(q);
+        })
+        .catch(function () {
+          console.log("please try agin later");
+        });
+    }
+  }
+
+  if (e.target.classList.contains("edit-phone")) {
+    let userInput = prompt("enter your new phone number");
+
+    let q = e.target.getAttribute("data-id");
+    axios
+      .post("/update-item", { phoneNo: userInput, id: q })
+      .then(function () {})
+      .catch(function () {
+        console.log("please try agin later");
+      });
+  }
+  if (e.target.classList.contains("edit-email")) {
+    let userInput = prompt("enter your new email");
+
+    let q = e.target.getAttribute("data-id");
+    axios
+      .post("/update-item", { mail: userInput, id: q })
+      .then(function () {})
+      .catch(function () {
+        console.log("please try agin later");
+      });
+  }
+});
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+  var y = document.getElementsByTagName("input");
+  var i;
+  for (i = 0; i < y.length; i++) {
+    y[i].value = "";
+  }
+}
